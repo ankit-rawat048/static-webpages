38e39f335cd2cb88e3befab750c3c90ed9acc6f6
ankit-rawat048 <ankitsinghrawt048@gmail.com>
2024-06-11 12:02:15 +0530

    page_2

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("i'm ankit rawat")
+let rawat = ["ankit", "singh", "rawat"];
+// for (let i=0 ;i< rawat.length; i++) {
+//     console.log(rawat[i]);
+// }
+
+
+for(let words of rawat) {
+    console.log(words);
+}
