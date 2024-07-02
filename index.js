6359e8b8cb6023cffdcaee45f96af1de5302aaf5
ankit-rawat048 <ankitsinghrawt048@gmail.com>
2024-07-02 18:19:31 +0530

    new day

--- index.js
+++ index.js
@@ -1,9 +1,2 @@
-let rawat = ["ankit", "singh", "rawat"];
-// for (let i=0 ;i< rawat.length; i++) {
-//     console.log(rawat[i]);
-// }
-
-
-for(let words of rawat) {
-    console.log(words);
-}
+let number =prompt("enter your mark");
+console.log(number);
